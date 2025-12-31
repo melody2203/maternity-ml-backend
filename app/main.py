d6270ac4f590884184f62cb4a8f62a6f84a56927
Merertu Philipose <merertuphilip@gmail.com>
2025-12-31 11:15:24 +0300

    Update main.py and remove requirements.txt

--- app/main.py
+++ app/main.py
@@ -3,12 +3,22 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Absolute path handling
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_DIR = os.path.join(BASE_DIR, "models")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://maternity-ml-frontend.vercel.app"],  # for testing, or use your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 log_model = joblib.load(os.path.join(MODEL_DIR, "logistic_model.pkl"))
 dt_model = joblib.load(os.path.join(MODEL_DIR, "decision_tree_model.pkl"))
@@ -18,7 +28,7 @@ class PatientData(BaseModel):
     Age: float
     SystolicBP: float
     DiastolicBP: float
-    BS: float
+    BloodSugar: float
     BodyTemp: float
     HeartRate: float
     MaternityMonth: int
@@ -26,7 +36,7 @@ class PatientData(BaseModel):
 @app.post("/predict/logistic")
 def predict_logistic(data: PatientData):
     X = np.array([[data.Age, data.SystolicBP, data.DiastolicBP,
-                   data.BS, data.BodyTemp, data.HeartRate,
+                   data.BloodSugar, data.BodyTemp, data.HeartRate,
                    data.MaternityMonth]])
     X_scaled = scaler.transform(X)
     pred = log_model.predict(X_scaled)[0]
@@ -35,7 +45,7 @@ def predict_logistic(data: PatientData):
 @app.post("/predict/tree")
 def predict_tree(data: PatientData):
     X = np.array([[data.Age, data.SystolicBP, data.DiastolicBP,
-                   data.BS, data.BodyTemp, data.HeartRate,
+                   data.BloodSugar, data.BodyTemp, data.HeartRate,
                    data.MaternityMonth]])
     pred = dt_model.predict(X)[0]
     return {"RiskLevel": int(pred)}
